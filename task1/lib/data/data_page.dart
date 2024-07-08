76971b504558e8b8d17a4c712ba5eb591c582ca2
Avin <108595642+avinsali@users.noreply.github.com>
2024-07-08 20:52:03 +0330

    first commit

---
+++ task1/lib/data/data_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class  _ArticleDescription extends StatelessWidget {
+  const _ArticleDescription({
+    required this.title,
+    required this.data,
+    required this.HR_AVG,
+    required this.sp_AVG,
+    required this.sprint,
+    required this.dis,
+  });
+  final String title;
+  final String data;
+  final String HR_AVG;
+  final String sp_AVG;
+  final String sprint;
+  final String dis;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Spacer(),
+        Row(
+          children: <Widget>[
+            Text(
+              data,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            Text(
+              title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+          ],
+        ),
+        const Spacer(),
+        const Padding(padding: EdgeInsets.only(bottom: 6.0)),
+        Row(
+          children: [
+            // Expanded(
+            Text('HR AVG :$HR_AVG % | spAVG :$sp_AVG m| sprint:$sprint | dis :$dis km', style: const TextStyle(fontSize: 11.0,color: Colors.black ) ,),
+            // ),
+          ],
+        ),
+        const Row(
+          children: [
+            Text('', style: TextStyle(fontSize: 12.0,color: Colors.black),),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CustomListItemTwo extends StatelessWidget {
+  const CustomListItemTwo({
+    super.key,
+    required this.thumbnail,
+    required this.title,
+    required this.data,
+    required this.HR_AVG,
+    required this.sp_AVG,
+    required this.sprint,
+    required this.dis ,
+  });
+
+  final Widget thumbnail;
+  final String title;
+  final String data;
+  final String HR_AVG;
+  final String sp_AVG;
+  final String sprint;
+  final String dis ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(7),
+          color: const Color.fromRGBO(184, 197, 208, 1.0)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 0.0),
+        child: SizedBox(
+          height: 100,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1.0,
+                child: thumbnail,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                  child:
+                  _ArticleDescription(
+                    title: title,
+                    data: data,
+                    HR_AVG: HR_AVG,
+                    sp_AVG: sp_AVG,
+                    sprint: sprint,
+                    dis : dis ,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ task1/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:task1/data/data_page.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'انتخاب بازیکن ')
+    );
+  }
+}
+class User {
+  final String title;
+  final String data;
+  final String HR_AVG;
+  final String sp_AVG;
+  final String sprint;
+  final String dis;
+  User(
+      this.title,
+      this.data,
+      this.HR_AVG,
+      this.sp_AVG,
+      this.sprint,
+      this.dis,
+      );
+}
+List user=[
+  User( 'پویان پرورش',   '1403/1/10',   '50', '5', '8' ,  '10'),
+  User( 'پویان پرورش',   '1403/1/8',   '50', '5', '8' ,  '10'),
+  User( 'پویان پرورش',   '1403/1/7',   '50', '5', '8' ,  '10'),
+  User( 'پویان پرورش',   '1403/1/3',   '50', '5', '8' ,  '10'),
+  User( 'پویان پرورش',   '1403/1/2',   '50', '5', '8' ,  '10'),
+
+
+];
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('                                     : انتخاب بازیکن ',style: TextStyle(fontSize: 25,color: Colors.black),),
+      ),
+      body: ListView.builder(
+        itemCount: user.length,
+        itemBuilder: (BuildContext context, int index) {
+          return  Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CustomListItemTwo(
+                thumbnail:Container(
+                    decoration:  const BoxDecoration(color:  Color.fromRGBO(6, 10, 25, 1.0),borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        bottomLeft: Radius.circular(16)
+                    ),
+                    ),
+                    child:
+                    const Column(
+                      children: [
+                        Text('', style: TextStyle(color: Colors.white,fontSize: 12.0),),
+                        Text('', style: TextStyle(color: Colors.white,fontSize: 12.0),),
+                        Icon(Icons.get_app_sharp,color: Colors.white,),
+                        Text('ذخیره کردن', style: TextStyle(color: Colors.white,fontSize: 12.0),)
+                      ],
+                    )
+                ),
+                title:user[index].title,
+                data:user[index].data,
+                HR_AVG:user[index].HR_AVG,
+                sp_AVG:user[index]. sp_AVG ,
+                sprint:user[index].sprint,
+                dis :user[index].dis
+            ),
+          );
+        },),
+    );// This trailing comma makes auto-formatting nicer for build methods.
+  }
+
+}
+
